2454284ce9f66344c7c0149cbe8a91133ba441ec
capf2030 <147984746+capf2030@users.noreply.github.com>
2025-09-21 04:25:41 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -15,18 +15,70 @@ sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
 echo "DISTRIB_SOURCECODE='immortalwrt'" >>package/base-files/files/etc/openwrt_release
 
-# Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-# sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
+# Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.6.1）
+sed -i 's/192\.168\.1\.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+
+# Modify default hostname (Change from 'OpenWrt' to 'Amlogical-Router')
+sed -i 's/OpenWrt/Amlogical-Router/g' package/base-files/files/bin/config_generate
+
+# Set timezone (Change to Asia/Shanghai)
+sed -i "s/'UTC'/'CST-8'/g" package/base-files/files/bin/config_generate
+
+# Set timezone name (Change to Beijing Time)
+sed -i "s/'UTC'/'Beijing Time'/g" package/base-files/files/bin/config_generate
 #
 # ------------------------------- Main source ends -------------------------------
 
 # ------------------------------- Other started -------------------------------
 #
-# Add luci-app-amlogic
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+# Add luci-app-amlogic (Amlogic device management interface)
+# svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+
+# Add luci-theme-argon (Modern theme)
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
+# Add luci-app-openclash (Clash client)
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+
+# Add luci-app-adguardhome (AD Guard Home)
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+
+# Add ddns-scripts_cloudflare (DDNS support)
+svn co https://github.com/openwrt/packages/trunk/net/ddns-scripts_cloudflare feeds/packages/net/ddns-scripts_cloudflare
+
+# Add automount (Auto mount USB drives)
+svn co https://github.com/openwrt/packages/trunk/utils/automount feeds/packages/utils/automount
+
+# Add diskman (Disk management)
+svn co https://github.com/lisaac/luci-app-diskman/trunk/applications/luci-app-diskman package/luci-app-diskman
+
+# Serverchan (WeChat notification)
+svn co https://github.com/tty228/luci-app-serverchan/trunk package/luci-app-serverchan
+
+# Add software package replacement options
+# coolsnowwolf default software package replaced with Lienol related software package
+# rm -rf feeds/packages/utils/{containerd,libnetwork,runc,tini}
+# svn co https://github.com/Lienol/openwrt-packages/trunk/utils/{containerd,libnetwork,runc,tini} feeds/packages/utils
+
+# Add third-party software packages (The entire repository)
+# git clone https://github.com/libremesh/lime-packages.git package/lime-packages
+
+# Add third-party software packages (Specify the package)
+# svn co https://github.com/libremesh/lime-packages/trunk/packages/{shared-state-pirania,pirania-app,pirania} package/lime-packages/packages
+
+# Add to compile options
+# sed -i "/DEFAULT_PACKAGES/ s/$/ pirania-app pirania ip6tables-mod-nat ipset shared-state-pirania uhttpd-mod-lua/" target/linux/armvirt/Makefile
 
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
+
+# Remove some default packages to save space
+rm -rf feeds/luci/applications/luci-app-vlmcsd
+rm -rf feeds/luci/applications/luci-app-upnp
+rm -rf feeds/luci/applications/luci-app-aria2
+
+# Add custom files to the image
+# cp -rf ../files/* ./
+
 #
 # ------------------------------- Other ends -------------------------------
-
